49cf730567c72dde3dee5d3b8b366de9cfb5f6fb
GMACCM\bahern <bill.ahern@berkadia.com>
2018-06-19 13:10:53 -0400

    enable compileOnSave, add updated pathing to ts file for server

--- server.ts
+++ server.ts
@@ -2,9 +2,12 @@ const express = require('express');
 const app = express();
 const path = require('path');
 app.get('/', (req: any, res: any) => {
-  res.sendFile(path.resolve('index.html'));
+  res.sendFile(path.resolve('app/index.html'));
 });
 app.get('/index.js', (req: any, res: any) => {
-  res.sendFile(path.resolve('index.js'));
+  res.sendFile(path.resolve('app/index.js'));
+});
+app.get('/index.css', (req: any, res: any) => {
+  res.sendFile(path.resolve('app/index.css'));
 });
 app.listen(8080, () => console.log('listening'));
